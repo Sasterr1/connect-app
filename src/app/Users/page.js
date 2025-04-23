af302a24809b109f338308371862f66666da431d
Alfian Ramadani <57712162+AlfianRamadani@users.noreply.github.com>
2025-04-23 08:48:03 +0800

    initial commit

---
+++ src/app/Users/page.js
@@ -0,0 +1,11 @@
+export default function Userspage() {
+    return[
+         <div>
+            <div>
+                <p>Halaman Users</p>
+            </div>
+            
+            
+        </div>
+    ]
+}
